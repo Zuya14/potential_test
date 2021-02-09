66d9d80bde917e1645c958ccb5bb77457061f43c
Zuya14 <s.ishizuya@gmail.com>
2021-02-09 13:42:51 +0900

    add some potential

--- potential_test.py
+++ potential_test.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import colors
 
+import math
+
 class PotentialField:
 
     def __init__(self, pos):
@@ -15,9 +17,11 @@ class PotentialField:
         temp = []
 
         for p in self.pos:
-            temp.append(self.calc_cost(x, y, p).unsqueeze(0))
+            # temp.append(self.calc_cost(x, y, p).unsqueeze(0))
+            temp.append(self.calc_RepulsivePotential(x, y, p, w=1.0, d_thr=0.3).unsqueeze(0))
 
-        temp.append(self.calc_cost(x, y, [-1.5, 0.0], w=-1.0).unsqueeze(0))
+        # temp.append(self.calc_cost(x, y, [-1.5, 0.0], w=-1.0).unsqueeze(0))
+        temp.append(self.calc_AttractivePotential(x, y, [-1.5, 0.0], w=1.0, d_thr=1.0).unsqueeze(0))
 
 
         print(torch.cat(temp, dim=0).size())
@@ -27,6 +31,33 @@ class PotentialField:
     def calc_cost(self, x, y, p, w=1.0):
         return w * torch.reciprocal(torch.norm(torch.tensor([(x - p[0])**2 , (y - p[1])**2])+1e-2, dim=0))
         # return w * torch.reciprocal(torch.norm(torch.tensor([(x - p[0])**2 , (y - p[1])**2])+5e-2, dim=0))
+
+    def calc_AttractivePotential(self, x, y, p, w=1.0, d_thr=1.0):
+        dx2 = (x - p[0])**2
+        dy2 = (y - p[1])**2
+        d = np.sqrt(dx2 + dy2)
+
+        result = np.where(d > d_thr, 
+            d_thr * w * d - w / 2.0 * d_thr**2, 
+            w / 2.0 * d**2
+            )
+
+        return torch.tensor(result)
+
+    def calc_RepulsivePotential(self, x, y, p, w=1.0, d_thr=1.0):
+        dx2 = (x - p[0])**2
+        dy2 = (y - p[1])**2
+        d = np.sqrt(dx2 + dy2)
+
+        result = np.where(d > d_thr, 0.0, w / 2.0 * (1.0/d + (1.0/d_thr))**2)
+
+        # if d > d_thr:
+        #     return torch.zeros(2)
+        # else:
+        #     # return w * torch.reciprocal(torch.norm(torch.tensor([(x - p[0])**2 , (y - p[1])**2])+1e-2, dim=0))
+        #     return w * torch.reciprocal(torch.norm(torch.tensor([dx , (y - p[1])**2])+1e-2, dim=0))
+        #     return torch.tensor(w / 2.0 * (1.0/d + (1.0/d_thr))**2)
+        return torch.tensor(result)
 
 if __name__ == '__main__':
 
@@ -157,8 +188,8 @@ if __name__ == '__main__':
 
     potentialField = PotentialField(obst_pos)
 
-    x = np.arange(-4, 4, 0.04) 
-    y = np.arange(-4, 4, 0.04)  
+    x = np.arange(-4, 4, 0.05) 
+    y = np.arange(-4, 4, 0.05)  
     x, y = np.meshgrid(x, y)
 
     z = potentialField.calc(x, y, 1.5, 0.0).numpy()
@@ -167,6 +198,6 @@ if __name__ == '__main__':
     ax = Axes3D(fig)
 
     my_cmap = plt.get_cmap('jet')
-    ax.scatter3D(x.reshape(-1), y.reshape(-1), z.reshape(-1), marker=".", c = (x + y + z).reshape(-1), cmap = my_cmap)
+    ax.scatter3D(x.reshape(-1), y.reshape(-1), z.reshape(-1), marker=".", c = (z).reshape(-1), cmap = my_cmap)
 
     plt.show()
